1a5273adf129732f4af12f92076793c29e606000
Adam Cumiskey <adam.cumiskey@gmail.com>
2016-12-25 13:20:06 -0500

    Initial Commit

---
+++ Mikado/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mikado
+//
+//  Created by Adam Cumiskey on 12/25/16.
+//  Copyright © 2016 adamcumiskey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
